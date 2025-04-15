7c4b2e791a38ee62228a245696ae076910c4b44b
guillermoGodoyPro <guillermo.rgo92@gmail.com>
2025-04-15 12:47:56 -0300

    Fin primera parte: Conexion a BD y API Base ok

---
+++ SistemaVenta.Model/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistemaVenta.Model;
+
+public partial class Usuario
+{
+    public int IdUsuario { get; set; }
+
+    public string NombreCompleto { get; set; } = null!;
+
+    public string Correo { get; set; } = null!;
+
+    public int? IdRol { get; set; }
+
+    public string Clave { get; set; } = null!;
+
+    public bool? EsActivo { get; set; }
+
+    public DateTime? FechaRegistro { get; set; }
+
+    public virtual Rol? IdRolNavigation { get; set; }
+}
