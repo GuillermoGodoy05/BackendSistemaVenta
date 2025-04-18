9318f3822c7f71b78ca6911241500a6a3b635771
guillermoGodoyPro <guillermo.rgo92@gmail.com>
2025-04-17 23:17:46 -0300

    Avances Utility 202504172317

---
+++ SistemaVenta.DTO/UsuarioDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVenta.DTO
+{
+    public class UsuarioDTO
+    {
+        public int IdUsuario { get; set; }
+        public string NombreCompleto { get; set; } = null!;
+        public string Correo { get; set; } = null!;
+        public int? IdRol { get; set; }
+        public string? RolDescripcion { get; set; }
+        public string Clave { get; set; } = null!;
+        public int? EsActivo { get; set; }
+    }
+}
